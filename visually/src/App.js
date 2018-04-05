403f78dfb282f8a147612d4fbba63dee17a2d0bb
erich <bucher.erich@gmail.com>
2018-04-04 23:20:22 -0700

    adds basic hamburger menu

--- visually/src/App.js
+++ visually/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {HeaderBar} from './headerBar.js'
+
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+		<HeaderBar></HeaderBar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,9 +19,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+		
       </div>
     );
   }
 }
+
+
+
 
 export default App;
--- visually/src/headerBar.js
+++ visually/src/headerBar.js
@@ -1,11 +1,53 @@
 import * as React from 'react';
 import './stylesheets/headerBar.css';
+import {Bootstrap, DropdownButton, MenuItem, ButtonToolbar, Button, } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import {addStyle} from 'react-bootstrap/lib/utils/bootstrapUtils.js';
+
+addStyle(DropdownButton, 'btn-hamburgerMenu');
+
+
+class HamburgerMenu extends React.Component{
+	render(){
+		return(
+			<div>
+				<ButtonToolbar>
+					<DropdownButton
+						bsStyle="hamburgerMenu"
+						id="dropdown-size-large"
+						noCaret
+						title={<span class="glyphicon glyphicon-menu-hamburger hamburgerMenuGlyph"></span>}
+					>
+						<MenuItem eventKey="1" href="index.html">
+							Home
+						</MenuItem>
+						<MenuItem eventKey="2" href="search.html">
+							Search
+						</MenuItem>
+						<MenuItem eventKey="3" href="graph.html">
+							Graphs
+						</MenuItem>
+						<MenuItem eventKey="4" href="settings.html">
+							Settings
+						</MenuItem>
+					</DropdownButton>
+				</ButtonToolbar>
+			</div>
+		)
+	}
+}
+
 
 export class HeaderBar extends React.Component {
   render() {
     return (
 		<div class="headerBar">
-			<span class="headerMainText">Visually</span>
+			<div class="hamburgerArea">
+				<HamburgerMenu/>
+			</div>
+			<div class="logoArea">
+				<span class="headerMainText">Visually</span>
+			</div>
 		</div>
 	)
   }
