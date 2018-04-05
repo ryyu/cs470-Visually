8dfc5bc0e85e08eda43f13b5834650d29079a6ff
erich <bucher.erich@gmail.com>
2018-04-04 17:40:05 -0700

    adds basic header bar

---
+++ visually/src/headerBar.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './stylesheets/headerBar.css';
+
+export class HeaderBar extends React.Component {
+  render() {
+    return (
+		<div class="headerBar">
+			<span class="headerMainText">Visually</span>
+		</div>
+	)
+  }
+}
