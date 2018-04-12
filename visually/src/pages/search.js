9cf548c8104f910d21198a025e6040480ee25a62
erich <bucher.erich@gmail.com>
2018-04-12 14:21:59 -0700

    Center search page contents

--- visually/src/pages/search.js
+++ visually/src/pages/search.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import {SearchBar} from '../searchBar.js';
 
+
+
 export class Search extends Component {
   render() {
     return (
-      <div>
-	  <h1>Search Page</h1>
-        <SearchBar/>
-      </div>
+		<div id="searchArea">
+			<h1>Search for...</h1>
+			<SearchBar/>
+		</div>
+
     );
   }
 }
