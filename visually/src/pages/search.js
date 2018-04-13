a607279db29481120a79e92c0ef3625ca28b1826
erich <bucher.erich@gmail.com>
2018-04-12 23:57:53 -0700

    Add tip below search bar

--- visually/src/pages/search.js
+++ visually/src/pages/search.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import {SearchBar} from '../searchBar.js';
+import '../stylesheets/pages/search.css';
+import infoIcon from '../assets/infoIcon.svg';
 
-
+var empty = {
+	height: "40%",
+	width: "100%",
+	display: "block"
+}
 
 export class Search extends Component {
   render() {
     return (
-		<div id="searchArea">
-			<h1>Search for...</h1>
+		<div class="searchArea">
+			<span class="titleText">Search for...</span>
 			<SearchBar/>
+			<br/>
+			<br/>
+			<br/>
+			<img src={infoIcon} height="20px"></img>
+			<span class="tipText">
+				Tip: If you can't find the profile that you are looking for, make sure 
+				it is a public account.
+			</span>
 		</div>
 
     );
