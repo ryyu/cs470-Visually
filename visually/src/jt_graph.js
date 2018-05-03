778d0d1f653c48be4877584c6aa94b3003d7dde8
Ryan Yu <yuryan45@gmail.com>
2018-05-02 19:19:19 -0700

    minor changes

--- visually/src/jt_graph.js
+++ visually/src/jt_graph.js
@@ -37,9 +37,8 @@ export class JTGraph extends React.Component {
                     options={{
                     	title: 'How Many Hashtags to Number of Likes',
                         //tooltip: {isHtml: true},
-                        vAxis: { gridlines: {count: 11}, title: "number of hashtags", baselineColor: 'none',
+                        vAxis: { gridlines: {count: 11}, title: "number of hashtags", baselineColor: 'none', },
                         //gridlineColor: '#70d8ff',
-                        textPosition: 'none'},
                         hAxis: {title: "number of likes", bold: false, },
                         bar: {groupWidth: '95%'},
                         legend: {position: 'none'},
