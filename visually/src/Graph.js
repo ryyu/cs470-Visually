90e92a22d0c901064aaf1dde3339561efc9dbe15
Ryan Yu <yuryan45@gmail.com>
2018-05-02 18:39:52 -0700

    added graph selector

--- visually/src/Graph.js
+++ visually/src/Graph.js
@@ -7,31 +7,57 @@ import locationIcon from './assets/locationIcon.svg';
 import hashtagIcon from './assets/hashtagIcon.svg';
 import {NavLink} from 'react-router-dom';
 import {Chart} from 'react-google-charts';
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
 
 export class Graph extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+          type: 'LikesTimeBC',
+        }
       }
+
+    handleChange = option => {
+      console.log("option", option.value);
+      this.setState({type: option.value});
+    }
+
+    RenderGraphs(text) {
+      console.log("rendering graphs")
+      const type = this.state.type;
+      console.log(text);
+      if (type === 'LikesTimeBC') {
+        return <BarChart width={500} height={500} range={text}/>
+      }
+      return <LineChart width={500} height={500} />
+    }
 
     render() {
         var name = qs.parse(this.props.location.search).text;
         var profilePic = qs.parse(this.props.location.search).src;
         var secondText = qs.parse(this.props.location.search).secText;
         console.log("secondText", secondText);
+        const options = ['LikesTimeBC', 'PostFrequencyLC'];
         return (
             <div className="graphWrapper">
                 <h1>Overview for: {name} </h1>
                 <img src={profilePic}/>
-                <BarChart width={500} height={500} range={secondText}/>
                 <br/>
-                <LineChart width={500} height={500} />
+                {this.RenderGraphs(secondText)}
+                <br/>
+                <Dropdown options={options} onChange={this.handleChange} value={options[0]} placeholder="Select an option" />
+                <br/>
+                <br/>
+                <br/>
+                <br/>
             </div>
         )
     }
-
 }
 
+/*
 export class BarChart extends React.Component {
   constructor(props){
       super(props)
@@ -47,7 +73,6 @@ export class BarChart extends React.Component {
     let parseText = () => {
         let temp = this.props.range;
         let ret = 1;
-        console.log("temp[3]", temp[3]);
         for(var i = 0; i < temp.length; i++) {
           if(temp[i] === 'm') {
             ret = 1000000;
@@ -67,7 +92,7 @@ export class BarChart extends React.Component {
     console.log("range", range);
     var times = [
     '00:00',
-    '2',
+    '01:00',
     '02:00',
     '03:00',
     '04:00',
@@ -193,8 +218,8 @@ export class BarChart extends React.Component {
       });
 
       svg.append("text")
-        .attr("x", (width / 2))
-        .attr("y", 0 - (margin.top / 2))
+        .attr("x", "10vw")
+        .attr("y", 0 - 15)
         .attr("text-anchor", "middle")
         .style("font-size", "16px")
         .style("text-decoration", "underline")
@@ -205,6 +230,107 @@ export class BarChart extends React.Component {
     render() {
         return <div className="graphResult"> </div>;
     }
+}
+*/
+export class BarChart extends React.Component {
+  constructor(props){
+      super(props)
+      this.chartEvents = [
+        {
+          eventName: 'select',
+          callback(Chart) {
+              // Returns Chart so you can access props and  the ChartWrapper object from chart.wrapper
+            console.log('Selected ', Chart.chart.getSelection());
+          },
+        },
+      ];
+      this.state = {
+        options: {
+          title: 'Number of Post Likes Sorted by Time Posted',
+          hAxis: { title: 'Time'},
+          vAxis: { title: 'Number of Likes'}, // minHeight, maxHeight
+          legend: 'none',
+          colors: ['#70d8ff'],
+        },
+        columns: [
+          {"label":"day","type":"string"},
+          {"label":"likes","type":"number"},
+        ],
+      };
+  }
+
+  generateData = () => {
+    let r = this.props.range;
+    let parseText = () => {
+        let temp = this.props.range;
+        let ret = 1;
+        for(var i = 0; i < temp.length; i++) {
+          if(temp[i] === 'm') {
+            ret = 1000000;
+          }
+        }
+        return ret;
+    };
+
+    let mod = parseText();
+    console.log("mod", mod);
+    let range = parseInt(r) * mod; // maybe need to multiply by some factor
+                             // 'm' => * 1,000,000 if js can handle numbers that big
+    if(range === NaN || range === 0) {
+      range = 100;
+    }
+
+    console.log("range", range);
+    var times = [
+    '00:00',
+    '01:00',
+    '02:00',
+    '03:00',
+    '04:00',
+    '05:00',
+    '06:00',
+    '07:00',
+    '08:00',
+    '09:00',
+    '10:00',
+    '11:00',
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00',
+    '23:00'];
+
+    var a = [];
+    for(var i = 0; i < 24; i++) {
+        var rand = Math.floor(Math.random() * range);
+        a.push([times[i], rand]);
+    }
+    console.log(a);
+    return a;
+  }
+
+  render() {
+      var data = this.generateData();
+      return <div className="BarResult">
+        <Chart
+        chartType="ColumnChart"
+        rows={data}
+        columns={this.state.columns}
+        options={this.state.options}
+        graph_id="BarChart"
+        width="100%"
+        height="300px"
+        chartEvents={this.chartEvents}
+        />
+      </div>;
+  }
 }
 
 export class LineChart extends React.Component {
@@ -224,37 +350,13 @@ export class LineChart extends React.Component {
       options: {
         title: 'Post Frequency for the month of April',
         hAxis: { title: 'Time'},
-        vAxis: { title: 'Number of Posts', minValue: 0, maxValue: 15 },
+        vAxis: { title: 'Number of Posts'}, // minHeight, maxHeight
         legend: 'none',
+        colors: ['#70d8ff'],
       },
-      rows: [
-        /*
-        [8, 12],
-        [4, 5.5],
-        [11, 14],
-        [4, 5],
-        [3, 3.5],
-        [6.5, 7],
-        */
-        [1949,11],[1949.08333333333,11],[1949.16666666667,13],[1949.25,12],[1949.33333333333,12],[1949.41666666667,13],[1949.5,14],[1949.58333333333,14],[1949.66666666667,136],[1949.75,119],[1949.83333333333,104],[1949.91666666667,118],[1950,115],[1950.08333333333,126],[1950.16666666667,141],[1950.25,135],[1950.33333333333,125],[1950.41666666667,149],[1950.5,170],[1950.58333333333,170],[1950.66666666667,158],[1950.75,133],[1950.83333333333,114],[1950.91666666667,140],[1951,145],
-        [1951.08333333333,150],[1951.16666666667,178],[1951.25,163],[1951.33333333333,172],[1951.41666666667,178],[1951.5,199],[1951.58333333333,199],[1951.66666666667,184],[1951.75,162],[1951.83333333333,146],[1951.91666666667,166],[1952,171],[1952.08333333333,180],[1952.16666666667,193],[1952.25,181],[1952.33333333333,183],[1952.41666666667,218],[1952.5,230],[1952.58333333333,242],[1952.66666666667,209],[1952.75,191],[1952.83333333333,172],[1952.91666666667,194],[1953,196],
-        [1953.08333333333,196],[1953.16666666667,236],[1953.25,235],[1953.33333333333,229],[1953.41666666667,243],[1953.5,264],[1953.58333333333,272],[1953.66666666667,237],[1953.75,211],[1953.83333333333,180],[1953.91666666667,201],[1954,204],[1954.08333333333,188],[1954.16666666667,235],[1954.25,227],[1954.33333333333,234],[1954.41666666667,264],[1954.5,302],[1954.58333333333,293],[1954.66666666667,259],[1954.75,229],[1954.83333333333,203],[1954.91666666667,229],[1955,242],
-        [1955.08333333334,233],[1955.16666666667,267],[1955.25,269],[1955.33333333334,270],[1955.41666666667,315],[1955.5,364],[1955.58333333334,347],[1955.66666666667,312],[1955.75,274],[1955.83333333334,237],[1955.91666666667,278],[1956,284],[1956.08333333334,277],[1956.16666666667,317],[1956.25,313],[1956.33333333334,318],[1956.41666666667,374],[1956.5,413],[1956.58333333334,405],[1956.66666666667,355],[1956.75,306],[1956.83333333334,271],[1956.91666666667,306],[1957,315],
-        [1957.08333333334,301],[1957.16666666667,356],[1957.25,348],[1957.33333333334,355],[1957.41666666667,422],[1957.5,465],[1957.58333333334,467],[1957.66666666667,404],[1957.75,347],[1957.83333333334,305],[1957.91666666667,336],[1958,340],[1958.08333333334,318],[1958.16666666667,362],[1958.25,348],[1958.33333333334,363],[1958.41666666667,435],[1958.5,491],[1958.58333333334,505],[1958.66666666667,404],[1958.75,359],[1958.83333333334,310],[1958.91666666667,337],[1959,360],
-        [1959.08333333334,342],[1959.16666666667,406],[1959.25,396],[1959.33333333334,420],[1959.41666666667,472],[1959.5,548],[1959.58333333334,559],[1959.66666666667,463],[1959.75,407],[1959.83333333334,362],[1959.91666666667,405],[1960,417],[1960.08333333334,391],[1960.16666666667,419],[1960.25,461],[1960.33333333334,472],[1960.41666666667,535],[1960.5,622],[1960.58333333334,606],[1960.66666666667,508],[1960.75,461],[1960.83333333334,390],[1960.91666666667,432]
-      ],
       columns: [
         {"label":"day","type":"number"},
         {"label":"number of posts","type":"number"},
-        /*
-        {
-          type: 'number',
-          label: 'Age',
-        },
-        {
-          type: 'number',
-          label: 'Weight',
-        },*/
       ],
     };
   }
@@ -266,7 +368,7 @@ export class LineChart extends React.Component {
 
     var a = [];
     for(var i = 0; i < days.length; i++) {
-        var rand = Math.floor(Math.random() * 25);
+        var rand = Math.floor(Math.random() * 5);
         a.push([days[i], rand]);
     }
     console.log('a',a);
@@ -285,9 +387,9 @@ export class LineChart extends React.Component {
         rows={data}
         columns={this.state.columns}
         options={this.state.options}
-        graph_id="ScatterChart"
+        graph_id="LineChart"
         width="100%"
-        height="400px"
+        height="300px"
         chartEvents={this.chartEvents}
         />
       </div>;
